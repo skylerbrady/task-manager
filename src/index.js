fb01c3f2ce42fa4e090a5ba5bbe7a092eebe1fa6
Akash Hegde <akash.hegde@accionlabs.com>
2020-06-18 23:33:05 +0530

    code updated

---
+++ src/index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+require("./db/mongoose");
+const userRouter = require("./routers/user");
+const taskRouter = require("./routers/task");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const multer = require("multer");
+const upload = multer({
+  dest: "images",
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(doc|docx)$/)) {
+      return cb(new Error("Please upload a Word document"));
+    }
+
+    cb(undefined, true);
+  },
+});
+app.post("/upload", upload.single("upload"), (req, res) => {
+  res.send();
+});
+
+app.use(express.json());
+app.use(userRouter);
+app.use(taskRouter);
+
+app.listen(port, () => {
+  console.log("Server is up on port " + port);
+});
+
+const bcrypt = require("bcrypt");
+
+const myFunction = async () => {
+  const password = "joeygetcode123!";
+  const hashedPassword = await bcrypt.hash(password, 8);
+
+  console.log(password);
+  console.log(hashedPassword);
+
+  const isMatch = await bcrypt.compare("joeygetcode123!", hashedPassword);
+  console.log(isMatch);
+};
+
+myFunction();
